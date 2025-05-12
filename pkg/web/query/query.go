eb9430f6b2ee8a9633f00e85e0e9490a252f4ea2
loikx <vart.leontii@gmail.com>
2025-05-13 00:00:05 +0300

    fill query struct data

--- pkg/web/query/query.go
+++ pkg/web/query/query.go
@@ -1,26 +1,90 @@
 package query
 
-import "sync"
+import (
+	"fmt"
+	"strings"
+	"sync"
+)
 
-type (
-	QueryName  string
-	QueryValue string
+const (
+	queryStarter   = '?'
+	querySeparator = '&'
+	queryEqual     = '='
 )
 
 type Query struct {
-	queryArgs map[QueryName]QueryValue
+	queryArgs map[string]string
 	mu        sync.RWMutex
 }
 
 func New(query map[string]string) *Query {
 	return &Query{
-		mu: sync.RWMutex{},
+		mu:        sync.RWMutex{},
+		queryArgs: query,
 	}
 }
 
-func (q *Query) Add(key, value string) {
+func (q *Query) Reset() {
+	q.mu.Lock()
+	q.queryArgs = make(map[string]string)
+	q.mu.Unlock()
 }
 
-func (q *Query) ToString() string {
-	return ""
+func (q *Query) Add(key, value string) {
+	q.mu.Lock()
+	q.queryArgs[key] = value
+	q.mu.Unlock()
+}
+
+func (q *Query) ToString() (string, error) {
+	if len(q.queryArgs) == 0 {
+		return "", nil
+	}
+
+	builder := strings.Builder{}
+	defer builder.Reset()
+
+	err := builder.WriteByte(queryStarter)
+	if err != nil {
+		return "", fmt.Errorf("can't write query starter to string: %w", err)
+	}
+
+	queryArgsCount := len(q.queryArgs)
+	position := 1
+	for queryName, queryValue := range q.queryArgs {
+		_, err = builder.WriteString(queryName)
+		if err != nil {
+			return "", fmt.Errorf("can't write query key(%s): %w", queryName, err)
+		}
+
+		err = builder.WriteByte(queryEqual)
+		if err != nil {
+			return "", fmt.Errorf("can't write query equal: %w", err)
+		}
+
+		_, err = builder.WriteString(queryValue)
+		if err != nil {
+			return "", fmt.Errorf("can't write query value(%s): %w", queryValue, err)
+		}
+
+		if position == queryArgsCount {
+			break
+		}
+		position++
+		err = builder.WriteByte(querySeparator)
+		if err != nil {
+			return "", fmt.Errorf("can't write query separator: %w", err)
+		}
+	}
+
+	return builder.String(), nil
+}
+
+func (q *Query) Build() (string, error) {
+	result, err := q.ToString()
+	if err != nil {
+		return result, fmt.Errorf("can't build queries: %w", err)
+	}
+	q.Reset()
+	return result, nil
 }
--- pkg/web/query/query_test.go
+++ pkg/web/query/query_test.go
@@ -1,1 +1,45 @@
 package query
+
+import (
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+)
+
+func TestQuery_ToString(t *testing.T) {
+	testCases := []struct {
+		name     string
+		query    *Query
+		expected string
+	}{
+		{
+			"no query keys",
+			New(map[string]string{}),
+			"",
+		},
+		{
+			"one query arg",
+			New(map[string]string{
+				"key1": "value1",
+			}),
+			"?key1=value1",
+		},
+		{
+			"many query arg",
+			New(map[string]string{
+				"key1": "value1",
+				"key2": "value2",
+				"key3": "value3",
+			}),
+			"?key1=value1&key2=value2&key3=value3",
+		},
+	}
+
+	for _, test := range testCases {
+		t.Run(test.name, func(t *testing.T) {
+			got, _ := test.query.ToString()
+
+			assert.Equal(t, test.expected, got)
+		})
+	}
+}
